ebfa151e38a8509395ee977c6c60dff27d8f99b1
anhtuan30012005@gmail.com <anhtuan30012005@gmail.com>
2024-03-23 09:50:53 +0700

    bài code xóa ký tự trắng thừa ở đầu cuối và giữa

--- delete_space_messi_1.cpp
+++ delete_space_messi_1.cpp
@@ -6,7 +6,7 @@ int main()
     // KHAI BÁO string:
     string name;
     getline(cin, name);
-
+    // biến đem số ký tự cần xóa 
     int dem = 0;
     int cuoi = 0;
     int i = 0;
@@ -14,7 +14,6 @@ int main()
     while (isspace(name[i]))
     {
         dem++;
-        // name.erase(name.begin());
     }
     name.erase(0, dem);
     // xóa ký tự trắng ở cuối
@@ -29,18 +28,18 @@ int main()
     for (int k = 1; k < name.length(); k++)
     {   
         if (isspace(name[k]) && isspace(name[k - 1]))
-        {   cout << k << endl;
+        {   
             name.erase(k, 1);
             k--;
-            cout << k << endl;
+            
         }
     }
-    // cout << "Số khoảng trắng là: " << dem << "  "
-    //      << "Và đây là kết quả: " << name << endl;
-    // cout << "Số khoảng trắng ở cuối là: " << cuoi << " "
-    //      << "Và đây là kết quả: "
-    //      << "'" << name << "'" << endl;
-    // cout << "Chuối thuần hóa là: "
-    //      << "'" << name << "'" << endl;
+    cout << "Số khoảng trắng là: " << dem << "  "
+         << "Và đây là kết quả: " << name << endl;
+    cout << "Số khoảng trắng ở cuối là: " << cuoi << " "
+         << "Và đây là kết quả: "
+         << "'" << name << "'" << endl;
+    cout << "Chuối thuần hóa là: "
+         << "'" << name << "'" << endl;
     return 0;
 }
